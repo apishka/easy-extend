45b0acdc86cab5f5ad9e552958319800746c411d
Eugene Reich <eugene.reich@gmail.com>
2015-06-04 14:00:48 +0400

    Some small fixes

--- source/Builder.php
+++ source/Builder.php
@@ -23,6 +23,26 @@ class Builder
     /**
      * Build
      *
+     * @param Event $event
+     * @access public
+     * @return void
+     */
+
+    public function buildFromEvent(Event $event)
+    {
+        // 1. Найти список нужных пакетиков
+        // 2. Обыскать пакетики на предмет апишечек
+        // 3. Создать файндеры для поиска файликов
+        // 4. Создать билдер и запилить кешик
+    }
+
+    public function buildFromCache()
+    {
+    }
+
+    /**
+     * Build
+     *
      * @access public
      * @return void
      */
